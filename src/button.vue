e9eef62a243b7d5a3fddb799972706f80f652b95
yuanmeng801 <yuanmeng801@outlook.com>
2021-02-04 23:52:40 +0800

    增加button组件；icon功能；引入parcel进行打包

---
+++ src/button.vue
@@ -0,0 +1,63 @@
+<template>
+  <button class="my-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot />
+    </div>
+  </button>
+</template>
+<script>
+export default {
+  props: ["icon", "iconPosition"],
+  props:{
+    icon:{},
+    iconPosition:{
+      type:String,
+      default:'left'
+    }
+  }
+};
+</script>
+<style lang="scss">
+.my-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: var(--font-size);
+  height: var(--button-height);
+  vertical-align: middle;
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+  .content {
+    order: 2;
+    line-height: var(--font-size);
+  }
+}
+.icon-right {
+  .icon {
+    margin-left: .3em;
+    margin-right: 0;
+    order: 2;
+  }
+  .content {
+    order: 1;
+  }
+}
+</style>
