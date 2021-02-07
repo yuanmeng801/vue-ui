b2d1ac23b0f831b0d9e85829a55bee5d0da7e320
yuanmeng0801 <1225610005@qq.com>
2021-02-07 17:58:00 +0800

    安装chai库

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
-
+import {assert} from 'chai'
 Vue.component('m-button', Button)
 Vue.component('m-icon',Icon)
 Vue.component('m-button-group',ButtonGroup)
@@ -10,3 +10,9 @@ Vue.component('m-button-group',ButtonGroup)
 new Vue({
   el: '#app',
 })
+
+const buttonConstructor = Vue.extend(Button)
+const button = new buttonConstructor({
+  
+})
+button.$mount()
